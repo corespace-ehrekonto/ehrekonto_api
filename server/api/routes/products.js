f2e4c06bbc3c2c63af165d1c1eef566893e288ab
AsP3X <zombymedia@gmail.com>
2021-12-17 13:29:45 +0100

    TASK: added patch and delete to products

--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -1,18 +1,45 @@
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
 
 console.log('Ehrekonto: products route loaded');
 
 // create the root product route
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling GET requests to /products'
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling POST requests to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'test') {
+        res.status(200).json({
+            message: 'Testing route for product id',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: "Handler doesn't support this product id",
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Removed product'
     });
 });
 
--- server/app.js
+++ server/app.js
@@ -11,12 +11,5 @@ const productRoutes = require('./api/routes/products');
 // Loading all available middlewares
 app.use('/products', productRoutes);
 
-// 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
-
 // Export the api handler
 module.exports = app;
