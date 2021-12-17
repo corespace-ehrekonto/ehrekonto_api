158eb4da5f1334a6ba1155424be5ce957e720e3f
AsP3X <zombymedia@gmail.com>
2021-12-17 14:15:56 +0100

    TASK: add root path module

---
+++ server/assets/utils/getProjectRoot.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const projectRoot = {}
+
+projectRoot.getDir = () => {
+    // return the project root directory path for the current project
+    return path.join(__dirname, '../../../');
+}
+
+module.exports = projectRoot;
