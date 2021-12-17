e30de335071771f0067ab7cc5119b28877e27226
AsP3X <zombymedia@gmail.com>
2021-12-17 14:16:18 +0100

    TASK: created error log handler for request

---
+++ server/assets/logging/errorHandler.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+// import custom modules
+const projectRoot = require('../utils/getProjectRoot');
+
+const errorHandlerLogger = {};
+
+errorHandlerLogger.log = (error, req, res, next) => {
+    const rootDir = projectRoot.getDir();
+
+    // create loggin stream for the api
+    const accessLogStream = fs.createWriteStream(path.join(rootDir, 'log', 'error.log'), {
+        flags: 'a'
+    });
+
+    // log the error
+    accessLogStream.write(`${new Date()} - ${req.method} ${req.originalUrl} - ${error.message} - ${error.status || 500}\n`);
+};
+
+module.exports = errorHandlerLogger;
