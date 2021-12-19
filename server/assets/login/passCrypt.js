f174d28c25468650f988d66ec77a984e7df5da39
AsP3X <zombymedia@gmail.com>
2021-12-19 21:07:21 +0100

    TASK: added encryption module

---
+++ server/assets/login/passCrypt.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+
+const passCrypt = {};
+
+passCrypt.encrypt = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(11), null);
+}
+
+passCrypt.decrypt = (password, hash) => {
+    return bcrypt.compareSync(password, hash);
+}
+
+module.exports = passCrypt;
