9f41c1d2f2b22d9906ea1836f151eb692f07e703
AsP3X <zombymedia@gmail.com>
2021-12-19 21:08:03 +0100

    TASK: added password hashing

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -5,13 +5,14 @@ const router = express.Router();
 
 // Import custom modules
 const errorHandlerLogger = require('../../assets/logging/errorHandler');
+const passCrypt = require('../../assets/login/passCrypt');
 
 console.log('Ehrekonto: users route loaded');
 
 // Import models
 const User = require('../models/users');
 
-// create the root order route
+// Get all users currently in the database
 router.get("/", (req, res, next) => {
     User.find()
       .exec()
@@ -27,30 +28,33 @@ router.get("/", (req, res, next) => {
       });
 });
 
+// Create a new user using the schema
 router.post("/", (req, res, next) => {
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-    });
+  const password = passCrypt.encrypt(req.body.password);
+  const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      username: req.body.username,
+      password: password,
+      email: req.body.email
+  });
 
-    user.save()
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message: "Handling POST requests to /products",
-          createdProduct: result
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
+  user.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST requests to /products",
+        createdProduct: result
       });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
+// Get user via id
 router.get('/:userID', (req, res, next) => {
     const id = req.params.userID;
     User.findById(id).exec()
@@ -64,6 +68,7 @@ router.get('/:userID', (req, res, next) => {
         });
 });
 
+// Get user via username
 router.get('/user/:username', (req, res, next) => {
     const username = req.params.username;
     User.find({username: username}).exec()
