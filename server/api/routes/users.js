d24c96054e817d90a8531c2c10f058f5b0a4e32a
AsP3X <zombymedia@gmail.com>
2021-12-19 11:36:45 +0100

    TASK: add user db handler
    Schema and user db handler

---
+++ server/api/routes/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+console.log('Ehrekonto: users route loaded');
+
+// Import models
+const User = require('../models/users');
+
+// create the root order route
+router.get("/", (req, res, next) => {
+    User.find()
+      .exec()
+      .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+});
+
+router.post("/", (req, res, next) => {
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email
+    });
+
+    user.save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "Handling POST requests to /products",
+          createdProduct: result
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+});
+
+module.exports = router;
