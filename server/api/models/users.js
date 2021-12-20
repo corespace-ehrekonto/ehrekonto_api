96d1413fc239291f5f713be86b7b50bc11fd4919
AsP3X <zombymedia@gmail.com>
2021-12-20 14:29:06 +0100

    TASK: added createdAt and updatedAt
    - Date of account creation added.
    - Updated Date added when update is made

--- server/api/models/users.js
+++ server/api/models/users.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	username: String,
-	password: String,
-	email: String
+  _id: mongoose.Schema.Types.ObjectId,
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updateAt: { type: Date, required: false },
+  img: { data: Buffer, contentType: String, required: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
