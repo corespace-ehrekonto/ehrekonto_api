c4bc4ed1e908253e52ef8c7c47394a3100e50526
AsP3X <zombymedia@gmail.com>
2022-03-06 21:44:15 +0100

    TASK: changed honorSchema to fit new implementation

--- server/api/models/honor.js
+++ server/api/models/honor.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const honorSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  UserId: { type: String, required: true },
+  IDFrom: { type: String, required: true },
+  IDTo: { type: String, required: true },
   HonorAmount: { type: Number, required: true },
   Comment: { type: String, required: false },
+  Date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Honor', honorSchema);
