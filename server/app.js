8ea82fde4cb959db7d2f5b1384dcaf4c08d941dc
AsP3X <zombymedia@gmail.com>
2022-04-24 21:49:55 +0200

    TASK: added new header options to the .env

--- server/app.js
+++ server/app.js
@@ -40,8 +40,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers',);
+  res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS);
+  res.header('Access-Control-Allow-Headers', process.env.ALLOWED_HEADERS);
+  res.header('Access-Control-Allow-Methods', process.env.ALLOWED_METHODS_X);
 
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', process.env.ALLOWED_METHODS);
